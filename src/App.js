67f3159ff4571df7faa43f6928fafb65b8270d0d
mjbaga <mjdbaga@gmail.com>
2022-02-03 13:58:42 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import DesignSystem from './pages/DesignSystem';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<h1>Homepage</h1>} />
+        <Route path='/design-system' element={<DesignSystem />} />
+      </Routes>
+      
     </div>
   );
 }
